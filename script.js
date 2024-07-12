e5c6bd25e808f580b716748a9dd4c0e7a6bc1f70
Pavan Magar <99827658+PavanMagar@users.noreply.github.com>
2024-07-13 00:32:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function openNav() {
+    document.getElementById("mySidebar").style.left = "0";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.left = "-1000px";
+  }
+  
