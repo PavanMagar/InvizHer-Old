157ca6607344fd920fc1ab2d236087fdbf7eb79c
Pavan Magar <99827658+PavanMagar@users.noreply.github.com>
2024-07-14 21:11:25 +0530

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,12 @@
+const articles = [
+    { title: 'First Article is here and there First Article is here and there', url: '/post/first-article.html' },
+    { title: 'How to do list in html. Get All Html Knowledge Here ', url: '/post/second-article.html' },
+    { title: 'How to do list in html. Get All Html Knowledge Here ', url: '/post/another-article.html' },
+    { title: 'First Article is here and there', url: '/post/first-article.html' },
+    { title: 'Second Article not my type to ', url: '/post/second-article.html' },
+    { title: 'Another ok no intbb ngh v ', url: '/post/another-article.html' },
+    { title: 'First Article is here and there', url: '/post/first-article.html' },
+    { title: 'Second Article not my type to ', url: '/post/second-article.html' },
+    { title: 'Another ok no intbb ngh v ', url: '/post/another-article.html' },
+    // Add more articles as needed
+];
