3d0e9c865d4a2928f0c0bc3a9334b2a9697e9978
Pavan Magar <99827658+PavanMagar@users.noreply.github.com>
2024-07-20 21:37:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,3 +208,26 @@ document.addEventListener('DOMContentLoaded', function () {
     checkURL();
     showInitialPosts();
 });
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "60px",
+    duration: 2500,
+    delay: 300,
+  });
+  
+  sr.reveal(".head", { origin:"top", delay: 300 });
+  sr.reveal(".posts", { delay: 300 });
+  sr.reveal(".post", { delay: 300 });
+  sr.reveal(".category", { delay: 300 });
+
+  sr.reveal(".article-header", { origin:"top", delay: 300 });
+  sr.reveal(".article-main", { origin:"bottom", delay: 400 });
+
+  sr.reveal(".article-share", { delay: 300 });
+  
+//   sr.reveal(".profile__info-group", { interval: 100, delay: 700 });
+//   sr.reveal(".profile__buttons", { delay: 800 });
+//   sr.reveal(".filters__content", { delay: 900 });
+//   sr.reveal(".filters", { delay: 1000 });
